e43031febcd99625310799191f2607d83f2017bd
oi <onno.invernizzi@ivention.nl>
2021-07-14 09:56:00 +0200

    Add README.MD to every project to describe what its intended purpose is.

--- Presentation/Attributes/ApiRouteAttribute.cs
+++ Presentation/Attributes/ApiRouteAttribute.cs
@@ -8,9 +8,11 @@ namespace Presentation.Attributes
 
     /// <summary>
     /// Defines an /api/route.
-    /// Extends RouteAttribute and sets the 'template' as the 'Name'.
+    /// Extends RouteAttribute and sets the 'template' as the 'Name'. This ensures the OpenAPI v3 json file contains
+    /// names we can base server call methods on in the client. These are automatically generated so it is very, very
+    /// important to set the name otherwise you'll get server call methods called postService1, postService2, etc.
     /// </summary>
-    /// <seealso cref="Microsoft.AspNetCore.Mvc.RouteAttribute" />
+    /// <seealso cref="RouteAttribute" />
     public class ApiRouteAttribute : RouteAttribute
     {
         /// <summary>
